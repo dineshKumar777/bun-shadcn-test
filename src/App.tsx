9162e81709bc8b4c682c48a0499b3df7008fcd49
Dinesh kumar <reachtodinesh@gmail.com>
2024-04-17 08:28:59 +0530

    initial working commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
-import { Button } from "./components/ui/button";
+import { useState } from 'react';
+import { Button } from './components/ui/button';
+import { AlertDialogDemo } from './components/shadcnComponents/AlertDialog';
+import { Separator } from './components/ui/separator';
+import { TableDemo } from './components/shadcnComponents/TableExample';
+import MDXContentExample from './input.mdx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,12 +14,22 @@ function App() {
 
   return (
     <>
-      <main className="container mx-auto p-8">
-        <p className="text-3xl">
+      <div>
+        <p className="text-sm font-medium leading-none">
           This is a simple Vite React Typescript starterpack
         </p>
-        <Button onClick={handleCountUpdate}>Inc Count: {count}</Button>
-      </main>
+        <Separator className="my-4" />
+        <MDXContentExample />
+        <Separator className="my-4" />
+
+        <div className="flex items-center space-x-2 text-sm">
+          <Button onClick={handleCountUpdate}>Inc Count: {count}</Button>
+          <Separator orientation="vertical" />
+          <AlertDialogDemo />
+        </div>
+        <Separator className="my-4" />
+        <TableDemo />
+      </div>
     </>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
- 
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import mdx from '@mdx-js/rollup';
+// import remarkGfm from 'remark-gfm';
+// import remarkPrism from 'remark-prism';
+import rehypePrettyCode from 'rehype-pretty-code';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    mdx({
+      // remarkPlugins: [remarkGfm, remarkPrism],
+      rehypePlugins: [rehypePrettyCode],
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
