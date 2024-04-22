a0c4e1968cbbba7bf712fa4f4bec86675af38666
Dinesh kumar <reachtodinesh@gmail.com>
2024-04-22 05:48:45 +0530

    added spider lottie animation

--- src/LottiePage.tsx
+++ src/LottiePage.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import animationData from './assets/spiderLottie.json';
 
 const LottiePage: React.FC = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
     <div className="container mx-auto px-4">
       <h1 className="text-3xl font-bold mb-8">About</h1>
       <p>simple lottie page</p>
+      <Lottie options={defaultOptions} height={400} width={400} />
     </div>
   );
 };
